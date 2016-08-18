8a5b5e8aa258e9b54b4126f49a86c884507aaeae
patevs <ppevans11@gmail.com>
2016-08-18 17:43:59 +1200

    Updates to CluedoGame class and added player hands

--- src/cluedo/model/CharacterToken.java
+++ src/cluedo/model/CharacterToken.java
@@ -1,19 +1,37 @@
 package cluedo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharacterToken extends Token {
 	
+	// field for the players unique ID
 	private final int Uid;
+	// stores the players chosen character token string
 	private final String characterStr;
+	// stores the players hand
+	private List<Card> hand;
 
 	public CharacterToken(String name, int playerUID, String characterStr) {
 		super(name);
 		this.Uid = playerUID;
 		this.characterStr = characterStr;
+		this.hand = new ArrayList<Card>();
+	}
+	
+	/**
+	 * Adds a card to the players hand
+	 * @param card
+	 */
+	public void addCard(Card card) {
+		hand.add(card);
 	}
 
 	/*
-	 * Getter methods
+	 * Getter and Setter methods
 	 */
 	public int getUid() { return Uid; }
 	public String getCharacterStr() { return characterStr; }
+	public List<Card> getHand(){ return hand; }
+	public void setHand(List<Card> newHand){ this.hand = newHand; }
 }
--- src/cluedo/model/CluedoGame.java
+++ src/cluedo/model/CluedoGame.java
@@ -1,6 +1,9 @@
 package cluedo.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import cluedo.view.CluedoBoard;
 
@@ -15,27 +18,114 @@ public class CluedoGame {
 	private CluedoBoard board;
 	// a list of all current players
 	private List<CharacterToken> activePlayers;
+	// stores the random solution to the game
+	private Card[] solution;
+	// stores the deck of cards for the game
+	private List<Card> deck;
+	// stores the unused cards once the all the cards are dealt
+	private List<Card> unusedCards;
 	
 	public CluedoGame(CluedoBoard board, List<CharacterToken> players) {
 		this.setBoard(board);
 		this.setActivePlayers(players);
+		
+		this.solution = initSolution();
+		this.deck = initDeck();
+		
+		dealCards();
+	}
+	
+	/**
+	 * This method gets a random solution to the Cluedo game.
+	 *  The solution contains one Character card, one Room card, 
+	 *  and one weapon card.
+	 * @return solution
+	 */
+	private Card[] initSolution() {
+		solution = new Card[3];
+		solution[0] = Character.getRandom();
+		solution[1] = Room.getRandom();
+		solution[2] = Weapon.getRandom();
+		return solution;
+	}
+
+	/**
+	 * Returns a list of all the cards in the game deck
+	 * 	excluding the solution cards
+	 * @return list of all cards
+	 */
+	private List<Card> initDeck() {
+		List<Card> deck = new ArrayList<Card>();
+		deck.addAll(Arrays.asList(Character.values()));
+		deck.addAll(Arrays.asList(Weapon.values()));
+		deck.addAll(Arrays.asList(Room.values()));
+		deck.removeAll(Arrays.asList(getSolution()));
+		return deck;
+	}
+
+	/**
+	 * Deals the cards evenly to all player leaving out left over cards
+	 */
+	private void dealCards() {
+		// remove unused cards from deck so remaining cards can be dealt evenly
+		int numUnused = deck.size() % numPlayers();
+		unusedCards = new ArrayList<Card>();
+		for(int i=0; i<numUnused; i++){
+			unusedCards.add(getCardFromDeck());
+		}
+		// deal cards evenly to players
+		int numCardsToDeal = deck.size() / numPlayers();
+		for(CharacterToken player: getPlayers()){
+			for(int i=0; i<numCardsToDeal; i++){
+				player.addCard(getCardFromDeck());
+			}
+		}
+	}
+	
+	/**
+	 * Gets a random card from the remaining deck of cards
+	 * @return random card from deck
+	 */
+	private Card getCardFromDeck() {
+		Card result = deck.get(new Random().nextInt(deck.size()));
+		deck.remove(result);
+		return result;
 	}
 
 	/*
 	 * Getter and Setter methods
 	 */
-	public CluedoBoard getBoard() {
-		return board;
+	public CluedoBoard getBoard() { return board; }
+	public void setBoard(CluedoBoard board) { this.board = board; }
+	public List<CharacterToken> getActivePlayers() { return activePlayers; }
+	public void setActivePlayers(List<CharacterToken> activePlayers) { this.activePlayers = activePlayers; }
+	/**
+	 * Returns the solution to the game
+	 * @return game solution
+	 */
+	private Card[] getSolution() {
+		return solution;
 	}
-	public void setBoard(CluedoBoard board) {
-		this.board = board;
+	/**
+	 * This method returns the current game deck
+	 * @return
+	 */
+	private List<Card> getDeck(){
+		return deck;
 	}
-	public List<CharacterToken> getActivePlayers() {
+	/**
+	 * Returns a list of active players in the game
+	 * @return list of active players
+	 */
+	private List<CharacterToken> getPlayers() {
 		return activePlayers;
 	}
-
-	public void setActivePlayers(List<CharacterToken> activePlayers) {
-		this.activePlayers = activePlayers;
+	/**
+	 * Returns the number of current players
+	 * @return number of current players
+	 */
+	private int numPlayers() {
+		return activePlayers.size();
 	}
 	
 	/*
